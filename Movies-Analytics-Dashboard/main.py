7c62ce13595ec20f900a2da1126b7b6c3e46ee8b
hassanoubrahim <65039956+hassanoubrahim@users.noreply.github.com>
2024-12-15 17:33:40 +0000

    test push

--- Movies-Analytics-Dashboard/main.py
+++
@@ -1,8 +0,0 @@
-
-from scrapFunctions import scrape, transform
-
-
-titles, years, ratings, genres, runtimes, imdb_ratings, metascores, votes = scrape()
-
-transform(titles, years, ratings, genres, runtimes, imdb_ratings, metascores, votes)
-
